c3aa78e3f553f9a5d56f2af629bec53a281f52d6
gabriel moraru <gabrielcmoraru@gmail.com>
2018-02-14 20:50:16 +0000

    updated to to es6 style and fixed missing div

--- scripts.js
+++ scripts.js
@@ -25,25 +25,39 @@ function refresh(){
 				const seen = dataChannels.updated_at.replace('T', ' ').replace('Z', ' ').slice(0,16);
 				const memberSince = dataChannels.created_at.slice(0,10);
 					//Append data to Content and add the offline class
-					$('.content').append("<div class='row generatedRow offline'><div class='col bg-success text-white'><div class='row'><div class='col-lg-2 col-md-3 col-12'><img class='img-fluid generatedImg' src='"
-					 + logo +
-					 "' alt='"
-					 + name +
-					 "'></div><div class='col'><h2>"
-					 + display_name +
-					 "</h2><cite class='hs'>"
-					 + status +
-					 "</cite></div><div><div class='row-12 row-lg-4 hs'><strong>Member Since: </strong>"
-					 + memberSince +
-					 "</div><div class='row-12 row-lg-4'><strong>Lastseen: </strong>"
-					 + seen +
-					 "</div></div></div><div class='row text-center'><div class='col hs'><strong>Followers: </strong>"
-					 + followers +
-					  "</div><div class='col'><button type='button' class='btn btn-sm btn-danger small'><a href='"
-					 + url +
-					  "' target='_blank' class='nav-item text-white'>OFFLINE</a></button></div><div class='col hs'><strong><i class='fa fa-eye'  aria-hidden='true'></i> Views: </strong>"
-					 + views +
-					   "</div></div></div>");
+					$('.content').append(`
+					  <div class='row generatedRow offline'>
+						<div class='col bg-success text-white'>
+						  <div class='row'>
+							<div class='col-lg-2 col-md-3 col-12'>
+							  <img class='img-fluid generatedImg' src='${logo}' alt='${name}'>
+							</div>
+							<div class='col'>
+							  <h2>${display_name}</h2><cite class='hs'>${status}</cite>
+							</div>
+							<div>
+							  <div class='row-12 row-lg-4 hs'>
+								<strong>Member Since: </strong>${memberSince}
+							  </div>
+							  <div class='row-12 row-lg-4'>
+								<strong>Lastseen: </strong>${seen}
+							  </div>
+							</div>
+						  </div>
+						  <div class='row text-center'>
+							<div class='col hs'><strong>Followers: </strong>${followers}</div>
+							<div class='col'>
+							  <button type='button' class='btn btn-sm btn-danger small'>
+								<a href='${url}' target='_blank' class='nav-item text-white'>OFFLINE</a>
+							  </button>
+							</div>
+							<div class='col hs'>
+							  <strong><i class='fa fa-eye' aria-hidden='true'></i> Views: </strong>${views}
+							</div>
+						  </div>
+						</div>
+					  </div>
+					`);
 					})
 			}
 			//If Channel is streaming use data
@@ -62,34 +76,46 @@ function refresh(){
 				const sFps = dataStreams.stream.average_fps.toPrecision(4);
 				const sLiveSince = dataStreams.stream.created_at.replace('T', ' ').replace('Z', ' ').slice(0,16);
 				//Append data to Content and add the online class
-				$('.content').append("<div class='row generatedRow online'><div class='col bg-success text-white'><div class='row'><div class='col-lg-2 col-md-3 col-12'><img class='img-fluid generatedImg' src='"
-					+ sLogo +
-					"' alt='"
-					+ sName +
-					"'></div><div class='col'><h2>"
-					+ sDisplay_name +
-					"</h2><cite class='hs'>"
-					+ sStatus +
-					"</cite><h3 class='hs'>Playing: </h3><blockqoute>"
-					+ sGame +
-					"</blockqoute><h4 class='hs'>Live since: </h4><blockqoute class='hs'>"
-					+ sLiveSince +
-					"</blockqoute></div><div class='col-12 col-md-4 col-lg-4'><div class='row'><img class=' hs img-fluid img-thumbnail mx-auto' src='"
-					+ sLivePreview +
-					 "' alt='"
-					+ sName +
-					 "'></div><div class='row'><p><i class='fa fa-eye' aria-hidden='true'></i><strong> LIVE</strong> | Viewers: "
-					+ sLiveViewers + "</p><div class='col'><p><i class='fa fa-film' aria-hidden='true'></i> "
-					+ sFps +
-					" <strong>FPS</strong></p></div></div></div<div class='col-lg-2 col-md-4 col-5'></div></div><div class='row text-center'><div class='col hs'><strong>Followers: </strong>"
-					+ sFollowers +
-					"</div><div class='col'><button type='button' class='btn btn-sm btn-primary'><a href='"
-					+ sUrl +
-					"' target='_blank' class='nav-item text-white'>WATCH ONLINE NOW</a></button></div><div class='col hs'><strong><i class='fa fa-eye' aria-hidden='true'></i> Views: </strong>"
-					+ sViews +
-					"</div></div></div>");
+				$('.content').append(`
+					<div class='row generatedRow online'>
+						<div class='col bg-success text-white'>
+							<div class='row'>
+								<div class='col-lg-2 col-md-3 col-12'>
+									<img class='img-fluid generatedImg' src='${sLogo}' alt='${sName}'>
+								</div>
+								<div class='col'>
+									<h2>${sDisplay_name}</h2><cite class='hs'>${sStatus}</cite>
+									<h3 class='hs'>Playing: </h3>
+									<blockqoute>${sGame}</blockqoute>
+									<h4 class='hs'>Live since: </h4>
+									<blockqoute class='hs'>${sLiveSince}</blockqoute>
+								</div>
+								<div class='col-12 col-md-4 col-lg-4'>
+									<div class='row'><img class=' hs img-fluid img-thumbnail mx-auto' src='${sLivePreview}' alt='${sName}'>
+									</div>
+									<div class='row'>
+										<div class='col'>
+											<p><i class='fa fa-eye' aria-hidden='true'></i><strong> LIVE</strong> | Viewers: ${sLiveViewers} </p>
+										</div>
+										<div class='col'><p><i class='fa fa-film' aria-hidden='true'></i>${sFps}<strong>FPS</strong></p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class='row text-center'>
+								<div class='col hs'><strong>Followers: </strong>${sFollowers}</div>
+								<div class='col'>
+									<button type='button' class='btn btn-sm btn-primary'><a href='${sUrl}' target='_blank' class='nav-item text-white'>WATCH ONLINE NOW</a>
+									</button>
+								</div>
+								<div class='col hs'>
+									<strong><i class='fa fa-eye' aria-hidden='true'></i> Views: </strong>${sViews}
+								</div>
+							</div>
+						</div>
+					</div>
+				`);
 			}
-
 		})
 	});
 }
